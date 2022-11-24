6a60c7475adf7fc738131323be9602ec841ee8dc
Asraful <asrafulislamcodes@gmail.com>
2022-11-25 00:16:18 +0600

    commited

--- components/CheckOutWizard.js
+++ components/CheckOutWizard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function CheckOutWizard({ activeStep = 0 }) {
+export default function CheckOutWizardPage({ activeStep = 0 }) {
   return (
     <ul className="mb-2 w-full steps">
       {['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(
---
+++ utils/data.js
@@ -0,0 +1,74 @@
+// import bcrypt from 'bcryptjs';
+
+// const data = {
+//   users: [
+//     {
+//       name: 'admin',
+//       email: 'admin@gmail.com',
+//       password: bcrypt.hashSync('123456'),
+//       isAdmin: true,
+//     },
+//     {
+//       name: 'asraful',
+//       email: 'asraful@gmail.com',
+//       password: bcrypt.hashSync('123456'),
+//       isAdmin: false,
+//     },
+//   ],
+//   product: [
+//     {
+//       name: 'Free Shirt',
+//       slug: 'free-shirt',
+//       category: 'Shirt',
+//       image:
+//         'https://images.pexels.com/photos/9604182/pexels-photo-9604182.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       price: 70,
+//       brand: 'Nike',
+//       rating: 4.5,
+//       numReviews: 8,
+//       countInStock: 20,
+//       description: 'A popular shirt',
+//     },
+//     {
+//       name: 'Fit Shirt',
+//       slug: 'fit-shirt',
+//       category: 'Shirt',
+//       image:
+//         'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       price: 70,
+//       brand: 'Adidas',
+//       rating: 3.5,
+//       numReviews: 10,
+//       countInStock: 20,
+//       description: 'A popular shirt',
+//     },
+//     {
+//       name: 'Slim Shirt',
+//       slug: 'slim-shirt',
+//       category: 'Shirt',
+//       image:
+//         'https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       price: 90,
+//       brand: 'Raymond',
+//       rating: 4.5,
+//       numReviews: 3,
+//       countInStock: 20,
+//       description: 'A popular shirt',
+//     },
+//     {
+//       name: 'Golf Pants',
+//       slug: 'golf-pants',
+//       category: 'Pants',
+//       image:
+//         'https://images.pexels.com/photos/7489197/pexels-photo-7489197.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       price: 90,
+//       brand: 'Oliver',
+//       rating: 2.5,
+//       numReviews: 13,
+//       countInStock: 4,
+//       description: 'A popular shirt',
+//     },
+//   ],
+// };
+
+// export default data;
